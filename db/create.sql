fc1ee22e1842e8037b22a82f13d39f0423c00699
KyanC09 <115638695+KyanC09@users.noreply.github.com>
2024-02-02 13:29:22 +0000

    database

--- db/create.sql
+++ db/create.sql
@@ -10,7 +10,8 @@ CREATE TABLE products (
   code VARCHAR(15),
   name VARCHAR(255),
   description TEXT,
-  price NUMERIC(10, 2)
+  price NUMERIC(10, 2),
+  releasedatum VARCHAR(50)
 );
 
 
@@ -23,13 +24,12 @@ CREATE TABLE products (
 -- want different data? check: https://www.mockaroo.com/910b6c20
 --
 
-insert into products (name, description, code, price) values ('The sno shoe', 'Mauris enim leo, rhoncus sed, vestibulum sit amet, cursus id.', '816905633-0', 10.5);
-insert into products (name, description, code, price) values ('Pizzi patta (members only)', 'Nulla ut erat id mauris vulputate elementum. Nullam varius.', '077030122-3', 11);
-insert into products (name, description, code, price) values ('The Etanol flippers ', 'Pellentesque at nulla. Suspendisse potenti.', '445924201-X', 13.5);
-insert into products (name, description, code, price) values ('Etanol Walkers , el pecador de la pradera', 'Duis bibendum, felis sed interdum venenatis, turpis enim blandit mi, in porttitor pede justo eu massa.', '693155505-7', 13.5);
-insert into products (name, description, code, price) values ('Kiss for Corliss, A (Almost a Bride)', 'Nulla ut erat id mauris vulputate elementum. Nullam varius. Nulla facilisi.', '686928463-6', 14);
-insert into products (name, description, code, price) values ('Velvet Goldmine', 'Nam ultrices, libero non mattis pulvinar, nulla pede ullamcorper augue, a suscipit nulla elit ac nulla.', '492662523-7', 14);
+insert into products (name, description, code, price, releasedatum) values ('The sno shoe', 'Mauris enim leo, rhoncus sed, vestibulum sit amet, cursus id.', '816905633-0', 10.5, 7-10-2023);
+insert into products (name, description, code, price, releasedatum) values ('Pizzi patta (members only)', 'Nulla ut erat id mauris vulputate elementum. Nullam varius.', '077030122-3', 11, 11-6-2022);
+insert into products (name, description, code, price, releasedatum) values ('The Etanol flippers ', 'Pellentesque at nulla. Suspendisse potenti.', '445924201-X', 13.5, 7-9-2023);
+insert into products (name, description, code, price, releasedatum) values ('Etanol Walkers , el pecador de la pradera', 'Duis bibendum, felis sed interdum venenatis, turpis enim blandit mi, in porttitor pede justo eu massa.', '693155505-7', 13.5, 9-11-2001);
+insert into products (name, description, code, price, releasedatum) values ('Air Pizzi 1, A (Almost a Bride)', 'Nulla ut erat id mauris vulputate elementum. Nullam varius. Nulla facilisi.', '686928463-6', 14, 11-9-2019 );
+insert into products (name, description, code, price, releasedatum) values ('Velvet Goldmine', 'Nam ultrices, libero non mattis pulvinar, nulla pede ullamcorper augue, a suscipit nulla elit ac nulla.', '492662523-7', 14, 19-2-2023);
 
-CREATE TABLE Sneakers (
-  id INTEGER PRIMARY KEY
+
   
